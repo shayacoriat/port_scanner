e87c59fd7c725e7b71bfa0ab188bcf251c12a203
shayacoriat <coriatshaya@gmail.com>
2022-11-30 23:29:21 -0500

    first commit for port scanner

---
+++ port with threading.py
@@ -0,0 +1,44 @@
+"""port scanner run from the command line ,
+ target ip as first sys.argv, 
+ amount of ports to be scanned as the second 
+sys.argv  
+
+uses threading to speed up the process 
+and queue's to ensure threads do not overlap """
+
+
+import sys
+import socket
+import threading
+from queue import Queue
+
+host = sys.argv[1]
+amount = int(sys.argv[2])
+q = Queue()
+
+
+
+def scan(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(.3)
+    result = s.connect_ex((host, port))
+    if result == 0:
+        print("port {} is open".format(port))
+
+
+def ports():
+    for port in range(amount):
+        q.put(port)
+
+
+def worker():
+    while not q.empty():
+        port = q.get()
+        scan(port)
+
+
+ports()
+for i in range(25):
+    thread = threading.Thread(target = worker)
+    thread.start()
+
